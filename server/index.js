c8f69f11dca0b77e80c9e7f0e276403c3d0d2eeb
Santiago Hernandez <sherna623@west-mec.org>
2024-10-01 17:02:52 -0700

    added server logic to run npm run dev

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// index.js
+// Required modules
+const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
+
+// Initialize Express application
+const app = express();
+
+// Define paths
+const clientPath = path.join(__dirname, '..', 'client/src');
+const dataPath = path.join(__dirname, 'data', 'users.json');
+const serverPublic = path.join(__dirname, 'public');
+// Middleware setup
+app.use(express.static(clientPath)); // Serve static files from client directory
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
+
+// Routes
+
+// Home route
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: clientPath });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
