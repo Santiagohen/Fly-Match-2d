1ffc92e22e4f090d91387e9340f0e98aa3e9e490
Myles Marshall <mmarsh319@west-mec.org>
2024-10-09 16:35:38 -0700

    basic functionality from myles branch

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
+const bcrypt = require('bcrypt');
 
 // Initialize Express application
 const app = express();
@@ -23,16 +24,102 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: clientPath });
 });
 
+
+app.get('/users', async (req, res) => {
+    try {
+        const data = await fs.readFile(dataPath, 'utf8');
+
+        const users = JSON.parse(data);
+        if (!users) {
+            throw new Error("Error no users available");
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Problem getting users" + error.message);
+        res.status(500).json({ error: "Problem reading users" });
+    }
+});
+
 app.get('/sign-up', (req, res) => {
-    res.sendFile('pages/sign-up.html', {root: serverPublic})
+    res.sendFile('pages/sign-up.html', { root: serverPublic })
 })
 
-app.post('/signed', async(req, res) => {
-    
-})
+app.post('/signed', async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        let users = []
 
+        try {
+            const data = await fs.readFile(dataPath, 'utf8');
+            users = JSON.parse(data);
+        } catch (error) {
+            console.error('Error reading user data:', error)
+            users = []
+        }
+
+        let user = users.find(u => u.email === email || u.username === username);
+
+        if (user) {
+            return res.status(409).send();
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10).then(hash => {
+            return hash;
+        }).catch(error => console.error(error.message));
+
+        let userId = users.length + 1;
+
+        user = { userId, admin: false, username, email, password: hashedPassword, bookedFlights: [] };
+        users.push(user);
+
+        await fs.writeFile(dataPath, JSON.stringify(users, null, 2))
+        return res.status(200).send();
+    } catch (error) {
+        console.error('error processing form:', error)
+    }
+});
+
+app.get('/sign-in', (req, res) => {
+    res.sendFile('pages/sign-in.html', { root: serverPublic })
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        let users = []
+
+        const data = await fs.readFile(dataPath, 'utf8');
+        users = JSON.parse(data);
+
+        let user = users.find(u => u.username === username);
+
+        if (user) {
+            // compare submitted password to stored hashed password, uses bcrypt built in function (I have no idea how it works)
+            const validatedLogin = await bcrypt.compare(password, user.password).then(res => {
+                return res;
+            }).catch(error => console.error(error.message));
+
+            if (validatedLogin) {
+                console.log("Valid login credentials");
+
+                // delete data the client should not store
+                delete user.password;
+                delete user.userId;
+
+                // send user object to client for storing in localstorage
+                res.status(200).send(user);
+            }
+        } else {
+            res.status(400);
+            res.redirect('sign-in')
+        }
+
+    } catch (error) {
+        console.error(error.message)
+    }
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
